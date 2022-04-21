0a8596286b75a3633575264de4ff554c5f45d9cf
olinemazumder <olinemozumder@gmail.com>
2022-04-21 09:56:49 +0600

    Frontend - Handled Form

--- frontend/src/components/Auth.js
+++ frontend/src/components/Auth.js
@@ -2,10 +2,29 @@ import React, {useState} from 'react';
 import {Box, Button, TextField, Typography} from "@mui/material";
 
 const Auth = () => {
+    const [user, setUser] = useState({
+        name: "",
+        email: "",
+        password: ""
+    })
     const [isSignup, setIsSignup] = useState(false)
+
+    const handleChange = event => {
+        const name = event.target.name;
+        const value = event.target.type === "checkbox" ? event.target.checked : event.target.value;
+        setUser({
+            ...user,
+            [name]: value
+        })
+    }
+    const handleSubmit = event => {
+        event.preventDefault();
+        console.log("New User: ",user);
+    }
+
     return (
         <div>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <Box
                     maxWidth={400}
                     display="flex"
@@ -25,13 +44,47 @@ const Auth = () => {
                     </Typography>
                     {
                         isSignup && (
-                            <TextField type="text" label="Name" placeholder="name" margin="normal"/>
+                            <TextField
+                                type="text"
+                                value={user.name}
+                                onChange={handleChange}
+                                name="name"
+                                label="Name"
+                                placeholder="name"
+                                margin="normal"
+                            />
                         )
                     }
-                    <TextField type="email" label="Email" placeholder="abc@email.com" margin="normal"/>
-                    <TextField type="password" label="Password" placeholder="password" margin="normal"/>
-                    <Button variant="contained" sx={{borderRadius: 1, marginTop: 1}}>Submit</Button>
-                    <Button onClick={() => setIsSignup(!isSignup)} sx={{borderRadius: 1}} color="warning">
+                    <TextField
+                        type="email"
+                        value={user.email}
+                        onChange={handleChange}
+                        name="email"
+                        label="Email"
+                        placeholder="abc@email.com"
+                        margin="normal"
+                    />
+                    <TextField
+                        type="password"
+                        value={user.password}
+                        onChange={handleChange}
+                        name="password"
+                        label="Password"
+                        placeholder="password"
+                        margin="normal"
+                    />
+                    <Button
+                        variant="contained"
+                        sx={{borderRadius: 1, marginTop: 1}}
+                        type="submit"
+                    >
+                        Submit
+                    </Button>
+                    <Button
+                        onClick={() => setIsSignup(!isSignup)}
+                        sx={{borderRadius: 1}}
+                        color="warning"
+                    >
                         {
                             isSignup ? "Change to Login" : "Change to Signup"
                         }
